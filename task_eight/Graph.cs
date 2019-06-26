0beeb9326cb41c896db7cb2124bbcaa0d1542467
stayl <staylimmk@gmail.com>
2019-06-26 18:42:20 +0500

    Добавьте файлы проекта.

---
+++ task_eight/Graph.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_eight
+{
+    public class Graph
+    {
+        public Vertex head;
+        public List<Vertex> Vertexes { get; set; }
+
+        public Graph(int number, int[,] matrix)
+        {
+            Vertexes = new List<Vertex>();
+            for (int i = 0; i < number; i++)
+            {
+                Vertex point = new Vertex(i + 1);
+                Vertexes.Add(point);
+            }
+            head = Vertexes[0];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    if (i != j && matrix[i, j] == 1)
+                    {
+                        if (Vertexes[i] != null) Vertexes[i].MakeTie(Vertexes[j]);
+                    }
+            }
+
+        }
+
+        public override string ToString()
+        {
+            string s = "Вершины: ";
+            foreach (Vertex peek in Vertexes)
+                s += $"\n{peek.ToString()}";
+            return s;
+        }
+
+        public void GoInDepth()
+        {
+            Vertex temp = head;
+           
+        }
+
+        public bool DeapthSearch(int v, int f, HashSet<string> bridges, HashSet<string> ribs, bool cycle, out int from)
+        {
+            from = f;
+            f = v;
+            Vertexes[v].colour++;
+            for (int i = 0; i < Vertexes.Count; i++)
+            {
+                bool k = ribs.Contains((v + 1).ToString() + (i + 1).ToString());
+                if (i != v && ribs.Contains((v + 1).ToString() + (i + 1).ToString()) && Vertexes[i].colour == Colour.white)
+                    cycle = DeapthSearch(i, f, bridges, ribs, cycle, out from);
+                if (i != v && i != from && ribs.Contains((i + 1).ToString() + (v + 1).ToString()) && Vertexes[i].colour == Colour.grey)
+                {
+                    from = i;
+                    return cycle = true;
+                }
+            }
+            return cycle;
+        }
+
+        public HashSet<string> RemoveCycleRibs(int v, int f, HashSet<string> ribs, out int from)
+        {
+            from = f;
+            f = v;
+            ribs.Remove((from + 1).ToString() + (v + 1).ToString());
+            ribs.Remove((v + 1).ToString() + (from + 1).ToString());
+            for (int i = 0; i < Vertexes.Count; i++)
+            {
+                if (i != v && (ribs.Contains((v + 1).ToString() + (i + 1).ToString()) || ribs.Contains((i + 1).ToString() + (v + 1).ToString())))
+                {
+                    //ribs.Remove((v + 1).ToString() + (i + 1).ToString());
+                    //ribs.Remove((i + 1).ToString() + (v + 1).ToString());
+                    RemoveCycleRibs(i, f, ribs, out from);
+                    if (v != from) return ribs;
+                }
+            }
+            return ribs;
+        }
+
+        //public HashSet<string> Bridges (int v)
+        //{
+
+        //}
+
+    }
+
+    public class Vertex
+    {
+        public Colour colour;
+
+        public List<Vertex> SubPoints { get; set; }
+
+        public string Name { get; set; }
+
+        public Vertex(int s)
+        {
+            colour = Colour.white;
+            Name = s.ToString();
+            SubPoints = new List<Vertex>();
+        }
+
+        public void MakeTie(Vertex point)
+        {
+            SubPoints.Add(point);
+        }
+
+        public int DepthSearch(string s)
+        {
+            int from = (int)this.colour, to = 1;
+            if (SubPoints.Count != 0 && (int)colour < 3)
+            {   
+                for (int i = 0; i < this.SubPoints.Count; i++)
+                {
+                    to = SubPoints[i].DepthSearch(s);
+                    colour++;
+                    if (from == to && to == 3) s += $"{this.Name}";
+                }
+            }
+            return to;
+        }
+
+        public override string ToString()
+        {
+            string s = Name.ToString() + ": связи с ";
+            foreach (Vertex n in SubPoints)
+                s += n.Name + " ";
+            return s;
+        }
+    }
+
+    public enum Colour { white  = 1, grey, black }
+}
---
+++ task_eight/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task_eight
+{
+    class Program
+    {
+        const int opt = 7;
+        static Random rand = new Random();
+        public static HashSet<string> Bridges = new HashSet<string>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа проверяет, является ли граф деревом.\n");
+            int[,] matrix = new int[1, 1];
+
+            Console.WriteLine(@"Выберите номер матрицы от 1 до " + opt);
+            int option = MenuOption();
+            switch (option)
+            {
+                case 1: matrix = matrix1; break;
+                case 2: matrix = matrix2; break;
+                case 3: matrix = matrix3; break;
+                case 4: matrix = matrix4; break;
+                case 5: matrix = matrix5; break;
+                case 6: matrix = matrix6; break;
+                case 7: matrix = matrix7; break;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Ваша матрица смежности:");
+            Console.ResetColor();
+            ShowMatrix(matrix);
+            
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                     if (i < j && matrix[i, j] == 1) Bridges.Add((i + 1).ToString() + (j + 1).ToString());
+
+            Graph oe = new Graph(matrix.GetLength(0), matrix);
+            
+            Console.WriteLine("\n" + oe.ToString());
+            Console.Write("\nРёбра:   ");
+            foreach (string s in Bridges) Console.Write(s + "    ");
+            Console.WriteLine();
+
+            int k;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(!oe.DeapthSearch(0, 0, new HashSet<string>(), Bridges, false, out k) ? "\nГраф является деревом" : "\nГраф не является деревом");
+            Console.ResetColor();
+            //Bridges = oe.RemoveCycleRibs(k, k, Bridges, out k);
+
+            //Console.WriteLine("\nМосты:   ");
+            //foreach (string s in Bridges) Console.Write(s + "    ");
+            Console.ReadKey();
+
+        }
+
+        static int[,] MakeMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j) matrix[i, j] = 0;
+                    else if (j > i)
+                    {
+                        int k = rand.Next(1, 10) % 2;
+                        matrix[i, j] = k;
+                        matrix[j, i] = k;
+                        if (k == 1) Bridges.Add((i + 1).ToString() + (j + 1).ToString());
+                    }
+                }
+            return matrix;
+        }
+
+        static void ShowMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write(matrix[i, j] + "  ");
+                Console.WriteLine();
+            }
+        }
+
+        static int MenuOption()
+        {
+            int option = 0;
+            bool alright = false;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write("Пункт:    ");
+                Console.ResetColor();
+                try
+                {
+                    option = Convert.ToInt32(Console.ReadLine());
+                    if (option < 1 || option > opt) ErrorInMenu();
+                    else alright = true;
+                }
+                catch (FormatException)
+                {
+                    ErrorInMenu();
+                    alright = false;
+                }
+            } while (!alright);
+
+            return option;
+        }
+
+        static void ErrorInMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Выберите пункт меню!");
+            Console.ResetColor();
+        }
+
+        static int[,] matrix1 = { { 0, 1, 0, 0 }, { 1, 0, 0, 1 }, { 0, 1, 0, 1 }, { 0, 0, 1, 0 } };
+        static int[,] matrix2 = { { 0, 1, 0, 0 }, { 1, 0, 1, 1 }, { 0, 1, 0, 1 }, { 0, 1, 1, 0 } };
+        static int[,] matrix3 = { { 0, 0, 0, 1 }, { 0, 0, 0, 1 }, { 0, 1, 0, 1 }, { 1, 0, 1, 0 } };
+        static int[,] matrix4 = { { 0, 1, 0, 0, 1, 0}, { 1, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 1 }, { 0, 0, 0, 0, 0, 1}, { 1, 0, 0, 0, 0, 1}, { 0, 0, 1, 1, 1, 0} };
+        static int[,] matrix5 = { { 0, 1, 0, 0, 0, 1 }, { 1, 0, 0, 1, 1, 0 }, { 0, 0, 0, 0, 1, 0 }, { 0, 1, 0, 0, 0, 0 }, { 1, 1, 1, 0, 0, 1 }, { 0, 0, 0, 0, 1, 0 } };
+        static int[,] matrix6 = { { 0, 0, 0, 0, 0, 1 }, { 0, 0, 0, 1, 1, 0 }, { 0, 0, 0, 0, 1, 0 }, { 0, 1, 0, 0, 0, 0 }, { 1, 1, 1, 0, 0, 1 }, { 0, 0, 0, 0, 1, 0 } };
+        static int[,] matrix7 = { { 0, 0, 1, 1, 1 }, { 0, 0, 1, 0, 0 }, { 1, 1, 0, 0, 0 }, { 1, 0, 0, 0, 1 }, { 1, 0, 0, 1, 0 } };
+
+    }
+}
